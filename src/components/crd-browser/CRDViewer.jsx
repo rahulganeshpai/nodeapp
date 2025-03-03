cd658d5a3b54850852b690a2bdf6904a99a8f013
rahulganeshpai <rahul.ganesh.pai@sap.com>
2025-03-03 11:49:45 +0100

    first commit

--- src/components/crd-browser/CRDViewer.jsx
+++ src/components/crd-browser/CRDViewer.jsx
@@ -7,9 +7,6 @@ import { SimpleTreeView } from "@mui/x-tree-view/SimpleTreeView";
 import { TreeItem, treeItemClasses } from "@mui/x-tree-view/TreeItem";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import examples from "../../utils/examples";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -21,13 +18,22 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import { useMediaQuery, useTheme } from "@mui/material";
 import Divider from "@mui/material/Divider";
+import examples from "../../utils/examples";
 
 const CustomTreeItem = styled(TreeItem)(({ theme }) => ({
   [`& .${treeItemClasses.content}`]: {
     padding: theme.spacing(0.5, 1),
     margin: theme.spacing(0.2, 0),
-    display: "flex", // Align children horizontally
+    display: "flex",
     alignItems: "center",
+    width: "auto",
+    backgroundColor: "transparent", // Ensure no background
+    "&:hover": {
+      backgroundColor: "transparent", // No hover color
+    },
+    "&.Mui-selected, &.Mui-selected:focus, &.Mui-selected:hover": {
+      backgroundColor: "transparent", // No background when selected or focused
+    },
   },
   [`& .${treeItemClasses.iconContainer}`]: {
     "& .close": {
@@ -36,8 +42,8 @@ const CustomTreeItem = styled(TreeItem)(({ theme }) => ({
   },
   [`& .${treeItemClasses.groupTransition}`]: {
     marginLeft: 15,
-    paddingLeft: 18,
-    borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
+    paddingLeft: 35,
+    borderLeft: `0.4px solid ${alpha(theme.palette.text.primary, 0.4)}`,
   },
 }));
 
@@ -54,10 +60,12 @@ function EndIcon(props) {
 }
 
 const BorderedTreeView = ({ crdData }) => {
-  console.log(crdData.spec.versions[0].schema.openAPIV3Schema.properties);
   const [isTreeVisible, setIsTreeVisible] = useState(true);
   const [isCardVisible, setIsCardVisible] = useState(false);
   const [isTable, setisTable] = useState(false);
+
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const toggleTreeVisibility = () => {
     setIsTreeVisible((prev) => !prev);
@@ -84,22 +92,20 @@ const BorderedTreeView = ({ crdData }) => {
     ),
     createData("upbound/provider-aws-kms", "Provider", "v1"),
   ];
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <div>
+    <div style={{ overflowX: "auto" }}>
       <br />
       <Stack
         direction={isSmallScreen ? "column" : "row"}
-        spacing={9} // Adjust spacing as needed for visual comfort
+        spacing={9}
         sx={{
-          padding: 2, // Padding around the entire stack
+          padding: 2,
           justifyContent: "flex-start",
           alignItems: "flex-start",
-          flexWrap: "wrap", // Ensures items wrap in smaller viewports
+          flexWrap: "wrap",
           "& > *": {
-            margin: "2px", // Customize the spacing here
+            margin: "2px",
           },
         }}
       >
@@ -132,168 +138,209 @@ const BorderedTreeView = ({ crdData }) => {
       <Divider />
       <br />
       <Stack spacing={2} direction="row">
-        <Button variant="text">README</Button>
-        <Button variant="text" onClick={toggleTreeVisibility}>
+        <Button variant="text" sx={{ color: "#2e7d32" }}>
+          README
+        </Button>
+        <Button
+          variant="text"
+          sx={{ color: "#2e7d32" }}
+          onClick={toggleTreeVisibility}
+        >
           Schema
         </Button>
-        <Button variant="text" onClick={toggleTableVisibility}>
+        <Button
+          variant="text"
+          sx={{ color: "#2e7d32" }}
+          onClick={toggleTableVisibility}
+        >
           DEPENDENCIES
         </Button>
-        <Button variant="text" onClick={toggleCardVisibility}>
+        <Button
+          variant="text"
+          sx={{ color: "#2e7d32" }}
+          onClick={toggleCardVisibility}
+        >
           Example
         </Button>
       </Stack>
       <br />
       {isCardVisible && (
-        <Card sx={{ minWidth: 275, maxWidth: 900, marginRight: "auto" }}>
-          <CardContent>
-            <Typography
-              gutterBottom
-              sx={{ color: "text.secondary", fontSize: 14 }}
-            >
-              <pre>{examples.generate_kms()}</pre>
-            </Typography>
-          </CardContent>
-        </Card>
+        <div>
+          <br />
+          <Typography
+            gutterBottom
+            sx={{ color: "text.secondary", fontSize: 14 }}
+          >
+            <pre>{examples.generate_kms()}</pre>
+          </Typography>
+          <br />
+          <br />
+        </div>
       )}
       {isTable && (
-        <TableContainer component={Paper}>
-          <Table
-            sx={{
-              tableLayout: "auto",
-              "& .MuiTableCell-root": {
-                border: "none", // Remove borders
-              },
-            }}
-            aria-label="simple table"
-          >
-            <TableHead sx={{ "background-color": "white" }}>
-              <TableRow>
-                <TableCell
-                  sx={{
-                    width: 420,
-                    backgroundColor: "white",
-                  }}
-                  align="left"
-                >
-                  Name
-                </TableCell>
-                <TableCell
-                  sx={{
-                    width: 420,
-                    backgroundColor: "white",
-                  }}
-                  align="left"
-                >
-                  Type
-                </TableCell>
-                <TableCell
-                  sx={{
-                    width: 420,
-                    backgroundColor: "white",
-                  }}
-                  align="left"
-                >
-                  Version
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{
-                    "&:last-child td, &:last-child th": { border: 0 },
-                  }}
-                >
+        <div>
+          <br />
+          <TableContainer>
+            <Table
+              sx={{
+                tableLayout: "auto",
+                border: "1px solid rgba(224, 224, 224, 1)",
+                "& .MuiTableCell-root": {
+                  border: "none",
+                },
+              }}
+              aria-label="simple table"
+            >
+              <TableHead sx={{ "background-color": "white" }}>
+                <TableRow>
                   <TableCell
-                    sx={{ backgroundColor: "white" }}
+                    sx={{ width: 420, backgroundColor: "white" }}
                     align="left"
-                    component="th"
-                    scope="row"
                   >
-                    {row.name}
+                    Name
                   </TableCell>
-                  <TableCell sx={{ backgroundColor: "white" }} align="left">
-                    {row.calories}
+                  <TableCell
+                    sx={{ width: 420, backgroundColor: "white" }}
+                    align="left"
+                  >
+                    Type
                   </TableCell>
-                  <TableCell sx={{ backgroundColor: "white" }} align="left">
-                    {row.fat}
+                  <TableCell
+                    sx={{ width: 420, backgroundColor: "white" }}
+                    align="left"
+                  >
+                    Version
                   </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell
+                      sx={{ backgroundColor: "white" }}
+                      align="left"
+                      component="th"
+                      scope="row"
+                    >
+                      {row.name}
+                    </TableCell>
+                    <TableCell sx={{ backgroundColor: "white" }} align="left">
+                      {row.calories}
+                    </TableCell>
+                    <TableCell sx={{ backgroundColor: "white" }} align="left">
+                      {row.fat}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <br />
+          <br />
+        </div>
       )}
       {isTreeVisible && (
-        <SimpleTreeView
-          aria-label="customized"
-          defaultExpandedItems={["1", "3"]}
-          slots={{
-            expandIcon: ExpandIcon,
-            collapseIcon: CollapseIcon,
-            endIcon: EndIcon,
-          }}
-          sx={{
-            overflowX: "hidden",
-            minHeight: 270,
-            flexGrow: 1,
-            maxWidth: 300,
-          }}
-        >
-          <CustomTreeItem itemId="1" label="spec">
-            <CustomTreeItem itemId="2" label="providerConfigRef">
-              Specifies how the provider creates, observes, updates, and deletes
-              resources
-              <CustomTreeItem itemId="21" label="aws">
-                Provider name for AWS
+        <div>
+          <SimpleTreeView
+            aria-label="customized"
+            defaultExpandedItems={["1", "3"]}
+            slots={{
+              expandIcon: ExpandIcon,
+              collapseIcon: CollapseIcon,
+              endIcon: EndIcon,
+            }}
+            sx={{
+              overflowX: "auto",
+              minHeight: 270,
+              flexGrow: 1,
+            }}
+          >
+            <CustomTreeItem itemId="1" label="spec">
+              <CustomTreeItem itemId="2" label="providerConfigRef">
+                Specifies how the provider creates, observes, updates, and
+                deletes resources
+                <CustomTreeItem itemId="21" label="aws">
+                  Provider name for AWS
+                </CustomTreeItem>
               </CustomTreeItem>
-            </CustomTreeItem>
-            <CustomTreeItem
-              itemId="3"
-              label={
-                <Box sx={{ display: "flex", alignItems: "center", gap: 6 }}>
-                  <Typography>resourceConfig</Typography>
-                  <Button
-                    variant="outlined"
-                    size="small"
+              <CustomTreeItem
+                itemId="3"
+                label={
+                  <Box
                     sx={{
-                      backgroundColor: "green",
-                      color: "white",
-                      padding: "1px 8px", // Adjust padding for a smaller button
-                      fontSize: "0.65rem", // Smaller font size
-                      "&.Mui-disabled": {
-                        backgroundColor: "green",
-                        color: "white",
-                        borderColor: "green",
-                      },
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      width: "100%",
                     }}
                   >
-                    Required
-                  </Button>
-                </Box>
-              }
-            >
-              ResourceConfig defines the general properties of this AWS
-              resource.
-              <CustomTreeItem itemId="31" label="tag">
-                Key-value map of resource tags.
+                    <Typography>resourceConfig</Typography>
+                    <Box sx={{ display: "flex", gap: 1 }}>
+                      <Button
+                        variant="outlined"
+                        size="small"
+                        sx={{
+                          backgroundColor: "green",
+                          color: "white",
+                          padding: "1px 8px",
+                          fontSize: "0.65rem",
+                          "&.Mui-disabled": {
+                            backgroundColor: "green",
+                            color: "white",
+                            borderColor: "green",
+                          },
+                        }}
+                      >
+                        Required
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        size="small"
+                        sx={{
+                          backgroundColor: "blue",
+                          color: "white",
+                          padding: "1px 8px",
+                          fontSize: "0.65rem",
+                          "&.Mui-disabled": {
+                            backgroundColor: "blue",
+                            color: "white",
+                            borderColor: "blue",
+                          },
+                        }}
+                      >
+                        Object
+                      </Button>
+                    </Box>
+                  </Box>
+                }
+              >
+                ResourceConfig defines the general properties of this AWS
+                resource.
+                <CustomTreeItem itemId="31" label="tag">
+                  Key-value map of resource tags.
+                </CustomTreeItem>
               </CustomTreeItem>
             </CustomTreeItem>
-          </CustomTreeItem>
-          <CustomTreeItem itemId="11" label="status">
-            {" "}
-            A Status represents the observed state.
-            <CustomTreeItem itemId="12" label="KeyArn">
-              KMS Key ARN.
+            <br />
+            <Divider />
+            <br />
+            <CustomTreeItem itemId="11" label="status">
+              A Status represents the observed state.
+              <CustomTreeItem itemId="12" label="KeyArn">
+                KMS Key ARN.
+              </CustomTreeItem>
+              <CustomTreeItem itemId="13" label="KeyAlias">
+                KMS Key Alias.
+              </CustomTreeItem>
             </CustomTreeItem>
-            <CustomTreeItem itemId="13" label="KeyAlias">
-              KMS Key Alias.
-            </CustomTreeItem>
-          </CustomTreeItem>
-        </SimpleTreeView>
+          </SimpleTreeView>
+        </div>
       )}
+      <br />
+      <br />
     </div>
   );
 };
